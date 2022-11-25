2da3a5040c1cc0c6715c9227d909c7d48a29645b
陈盼 <chenpan.cp@dsphere.cn>
2022-11-25 10:28:47 +0800

    提及哦啊

--- src/Vague.js
+++ src/Vague.js
@@ -39,30 +39,6 @@ const App = () => {
       </Form.Item>
 
       <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
         wrapperCol={{
           offset: 8,
           span: 16,
